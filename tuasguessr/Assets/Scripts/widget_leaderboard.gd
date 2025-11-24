f8893058eed3d0e7a1d18563a324a3f4405587b0
Motto73 <alexille.postia@gmail.com>
2025-11-24 12:12:13 +0200

    adasd

--- tuasguessr/Assets/Scripts/widget_leaderboard.gd
+++ tuasguessr/Assets/Scripts/widget_leaderboard.gd
@@ -10,9 +10,6 @@ func set_data(dat: Dictionary):
 	#Reparent to instanly move the loading thingamabob away, then queue deletion
 	loading.reparent(self)
 	loading.queue_free()
-	#TODO - I still don't know what format the data is in, so I use this dummy for now
-	dat = {"name":"yo", "points":5 }
-	print(dat)
 	for data in dat:
 		var name = data[0]
 		var pts = data[1]
