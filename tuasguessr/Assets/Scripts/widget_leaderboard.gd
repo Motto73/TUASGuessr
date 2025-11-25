3618bf20376a6084566da4e0f3d20e340d6daae5
Motto73 <alexille.postia@gmail.com>
2025-11-25 14:38:05 +0200

    Leaderboard numbering

--- tuasguessr/Assets/Scripts/widget_leaderboard.gd
+++ tuasguessr/Assets/Scripts/widget_leaderboard.gd
@@ -13,9 +13,11 @@ func set_data(dat: Array):
 	#Reparent to instanly move the loading thingamabob away, then queue deletion
 	loading.reparent(self)
 	loading.queue_free()
+	var num = 1
 	for entry in dat:
 		var name = entry.get("username", "Unknown")
 		var pts = entry.get("points", 0)
 		var tag = load("res://Resource Scenes/scoretag.tscn").instantiate() as Label
-		tag.text = str(name) + " : " + str(pts)
+		tag.text = str(num) + ". " + str(name) + " : " + str(pts)
+		num += 1
 		box.add_child(tag)
