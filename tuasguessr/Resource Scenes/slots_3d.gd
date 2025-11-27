dcebc2fd01a12efe6a8ad71d44cd0e3646ffa27c
Motto73 <alexille.postia@gmail.com>
2025-11-27 19:01:51 +0200

    Little shop man is now happy

--- tuasguessr/Resource Scenes/slots_3d.gd
+++ tuasguessr/Resource Scenes/slots_3d.gd
@@ -39,11 +39,19 @@ func _ready():
 	#Slot setup
 	state = "ready"
 	set_random_images()
+	#Shop setup
+	print(sellerguy)
+	face_mat = sellerguy.get_active_material(1) as Material
+	face_mat.albedo_texture = face_normal
+	animator.animation_finished.connect(_on_animation_finished)
+	animator.animation_started.connect(_on_animation_started)
 
 
 func _process(delta):
 	#Process slots
 	process_slots(delta)
+	#Process shop
+	process_shop(delta)
 	#Process moving the camera
 	if show_shop:
 		movetimer += delta
@@ -93,6 +101,11 @@ func _input(event):
 					_on_roll_button_button_down()
 				if cast.collider.get_parent() == shopswitch or cast.collider.get_parent() == buttonring:
 					lit(!show_shop)
+			if cast and cast.collider.get_parent() is ShopItem:
+				var si = cast.collider.get_parent()
+				if Game.Active.actualGame.buy_item(si):
+					si.queue_free()
+					play_anim(randfrom(yippees))
 
 func raycast() -> Dictionary:
 	var mouse = subview.get_mouse_position()
@@ -201,3 +214,93 @@ func process_slots(delta):
 			timer = 0
 			Game.Active.actualGame.allow_bets()
 			led_display.set_diff(selectedPoint.difficulty)
+
+#Shop behavior
+@export_category("Shop")
+@export var Slots : Array[Node3D]
+@export var ItemData : ItemDataPoints
+
+@onready var animator := $SlopMachine/AnimationPlayer
+@onready var sellerguy := $"SlopMachine/Armature/Skeleton3D/Seller man guy"
+
+var face_normal = preload("res://Assets/Textures/face_normal.png")
+var face_joy = preload("res://Assets/Textures/face_joy.png")
+var face_angry = preload("res://Assets/Textures/face_angry.png")
+var face_suprise = preload("res://Assets/Textures/face_surprise.png")
+var face_mat : Material
+
+var empty := true
+var isvisible := false
+var saidhello := false
+
+var idles = ["Idle_0", "Idle_1", "Idle_2"]
+var idleboreds = ["Idle_Bored_0", "Idle_Bored_1", "Idle_Bored_2", "Idle_Bored_3"]
+var idlerares = ["Idle_Rare_0"]
+var yippees = ["Yippee_0", "Yippee_1"]
+
+var exp_angries = ["Slide_R", "Slide_L"]
+var exp_happies = ["Yippee_0", "Yippee_1", "Hello_0"]
+var exp_normals = ["Idle_0", "Idle_1", "Idle_2", "Idle_Bored_0", "Idle_Bored_1", "Idle_Bored_2", "Idle_Bored_3", "Idle_Rare_0"]
+
+func process_shop(delta):
+	if !animator.is_playing():
+		queue_anim(randomidle())
+	if Game.Active.actualGame != null && empty:
+		load_items()
+
+#Shop animations
+func play_anim(aname):
+	animator.play(aname)
+
+func queue_anim(aname):
+	#animator.clear_queue()
+	animator.queue(aname)
+
+func _on_animation_finished(aname):
+	if aname == "Slide_R" && !saidhello:
+		queue_anim("Hello_0")
+		saidhello = true
+		
+func _on_animation_started(name):
+	if exp_angries.has(name):
+		face_mat.albedo_texture = face_angry
+	elif exp_happies.has(name):
+		face_mat.albedo_texture = face_joy
+	elif exp_normals.has(name):
+		face_mat.albedo_texture = face_normal
+
+func randomidle():
+	var num = randf()
+	if num <.2:
+		return randfrom(idles)
+	elif num < .95:
+		return randfrom(idleboreds)
+	else:
+		return "Idle_Rare_0"
+
+func randfrom(list):
+	return list[randi_range(0, len(list) - 1)]
+
+#Shop items
+func load_items():
+	empty = false
+	var items := []
+	#Create a list of items that are allowed in the store
+	for item in ItemData.Items:
+		if not Game.Active.actualGame.inventorytags.has(item.tag):
+			items.append(item)
+	#Randomize
+	Slots.shuffle()
+	items.shuffle()
+	#Clear slots
+	for x in Slots:
+		for y in x.get_children():
+			y.queue_free()
+	#Spawn items
+	for i in len(Slots):
+		if i >= len(items):
+			break
+		var slot = Slots[i]
+		var item = items[i].scene.instantiate()
+		slot.add_child(item)
+		item.position = Vector3.ZERO
