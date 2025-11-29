85c4b1f9eb277da67f63685ac88d53cf202c0d73
TechTuomas <tumppi.uotila@gmail.com>
2025-11-29 21:15:53 +0200

    increased version

--- tuasguessr/Assets/Scripts/game.gd
+++ tuasguessr/Assets/Scripts/game.gd
@@ -18,7 +18,7 @@ var mainScene : Node
 var popup : Node
 
 ##This is the primary build version. Whenever releasing a new build, change this.
-var gameVersion : String = "0.0.7"
+var gameVersion : String = "0.0.8"
 var osname : String
 var osmode : String
 var osmobile : bool
